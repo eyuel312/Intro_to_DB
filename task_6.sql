3e48fef31f5e912da667726ade92d6991dd62b86
Eyuel A <gg7qhqvvt4@privaterelay.appleid.com>
2025-12-13 14:21:04 +0300

    Refactor customer insert statements for bulk insert

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,5 @@
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.');
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+INSERT INTO CUSTOMERS (customer_name, email, address)
+VALUES 
+('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
