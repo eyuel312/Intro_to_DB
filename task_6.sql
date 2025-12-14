6a2fbb5fea1db1ceb334984ac503c8efe1fb8299
Eyuel A <gg7qhqvvt4@privaterelay.appleid.com>
2025-12-14 16:47:53 +0300

    included customer_id which was left out

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
-INSERT INTO CUSTOMERS (customer_name, email, address)
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
 VALUES 
-('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+('2','Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+('3','Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+('4','Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
